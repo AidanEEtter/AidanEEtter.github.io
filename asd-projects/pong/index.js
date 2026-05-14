1068d01240ecd1012d9aa870f21c657ee6e02801
AidanEEtter <Elwin-364@wearescpps.org>
2026-05-14 23:09:25 +0000

    finished pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -39,27 +39,78 @@ function runProgram() {
     gameItem.height = $(id).height();
     return gameItem;
   }
+
+  var scoreLeft = 0
+  var scoreRight = 0
+
+  // initialize score display
+  $("#player1Score").text(scoreLeft)
+  $("#player2Score").text(scoreRight)
+
   var ball = gameItem("#ball", 0, 0);
   var leftPaddle = gameItem("#leftPaddle", 0, 0);
   var rightPaddle = gameItem("#rightPaddle", 0, 0);
   function startBall() {
-    var randomNumX = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1);
+   var randomNumX = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1);
     ball.speedX = randomNumX;
     console.log(ball.speedX)
     var randomNumY = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1);
     ball.speedY = randomNumY;
      console.log(ball.speedY)
+    ball.x = BOARD_WIDTH / 2
+    ball.y = BOARD_HEIGHT / 2
+  }
+  function startPaddle() {
+    leftPaddle.x = 0
+    rightPaddle.x = BOARD_WIDTH - rightPaddle.width
   }
 
   function wallCollision(id) {
-    if (id.x > BOARD_WIDTH || id.x < 0) {
-      
+    if (id.y + id.height > BOARD_HEIGHT) {
+     if (id === ball) {
+      id.speedY = id.speedY * -1
+     }
+     else {
+      id.y -= id.speedY
+     }
+    }
+    else if (id.y < 10) {
+     if (id === ball) {
+      id.speedY = id.speedY * -1
+     }
+     else {
+      id.y -= id.speedY
+     }
+    }
+  }
+
+  function doCollide(obj1, obj2) {
+    if (obj1.x < obj2.x + obj2.width && obj1.x + obj1.width > obj2.x && obj1.y < obj2.y + obj2.height && obj1.y + obj1.height > obj2.y) {
+      return true
+    }
+      else {
+        return false
+      }
+  }
+
+
+  function ballScore() {
+    if (ball.x < 0){
+      scoreRight += 1
+      $("#player2Score").text(scoreRight)
+      startBall()
+    }
+    else if (ball.x > BOARD_WIDTH - ball.width) {
+      scoreLeft += 1
+      $("#player1Score").text(scoreLeft)
+      startBall()
     }
   }
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL); // execute newFrame every 0.0166 seconds (60 Frames per second)
   startBall();
+  startPaddle()
 
   // change 'eventType' to the type of event you want to handle
 
@@ -74,7 +125,22 @@ function runProgram() {
   function newFrame() {
     moveObject(ball);
     moveObject(leftPaddle);
-    moveObject(rightPaddle)
+    moveObject(rightPaddle);
+    wallCollision(leftPaddle)
+    wallCollision(rightPaddle)
+    wallCollision(ball)
+    ballScore()
+    if (doCollide(ball, leftPaddle) === true) {
+      ball.speedX = ball.speedX * -1
+    }
+    if (doCollide(ball, rightPaddle) === true) {
+      ball.speedX = ball.speedX * -1
+    }
+    if (scoreLeft === 10 || scoreRight === 10){
+      endGame()
+    }
+    }
+    
   }
 
   /* 
@@ -88,7 +154,8 @@ function runProgram() {
     } else if (event.which === KEYCODE.DOWNLEFT) {
       //console.log("S pressed");
       leftPaddle.speedY = 10
-    } else if (event.which === KEYCODE.UPRIGHT) {
+    }
+      if (event.which === KEYCODE.UPRIGHT) {
       //console.log("UP pressed");
       rightPaddle.speedY = -10
     } else if (event.which === KEYCODE.DOWNRIGHT) {
